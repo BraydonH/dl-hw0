f75f01620334eb4968a18c33f142ff9fc063bd12
BraydonHall <BraydonH@cs.washington.edu>
2018-10-15 17:03:02 -0700

    Matmul fix

--- src/connected_layer.c
+++ src/connected_layer.c
@@ -78,7 +78,7 @@ void backward_connected_layer(layer l, matrix prev_delta)
         // Finally, if there is a previous layer to calculate for,
         // calculate dL/d(in). Again, using axpy, add this into the current
         // value we have for the previous layers delta, prev_delta.
-        axpy(1, matmul(delta, l.w), prev_delta)
+        axpy(1, matmul(delta, l.w), prev_delta);
     }
 }
 
